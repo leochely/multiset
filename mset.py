7350cf4d0a244092c8dfe20f866d7110c2c9df45
Léo Chely <31514354+leochely@users.noreply.github.com>
2017-11-02 09:20:48 -0600

    Create mset.py

---
+++ mset.py
@@ -0,0 +1,39 @@
+class MultiSet(object):
+    dico = {}
+    
+    def __init__(self, data):
+        if type(data) == list:
+            for value in data:
+                if type(value) == tuple:
+                    self.dico[value[0]] = value[1]
+                else:
+                    if value in dico:
+                        self.dico[value] += 1
+                    else:
+                        self.dico[value] = 1
+        elif type(data) == tuple:
+            for value in data:
+                if value in dico:
+                    self.dico[value] += 1
+                else:
+                    self.dico[value] = 1
+        elif type(data) == set:
+            for value in data:
+                if value in dico:
+                    self.dico[value] += 1
+                else:
+                    self.dico[value] = 1
+        elif type(data) = dict:
+            self.dico = data
+        else:
+            erreurs = 0
+            for value in data:
+                if(value.isalpha()):
+                    if value in dico:
+                        self.dico[value] += 1
+                    else:
+                        self.dico[value] = 1
+                else:
+                    erreurs += 1
+            if erreurs > 0:
+                print "Ignoring " + erreurs + " out of " + len(data)
